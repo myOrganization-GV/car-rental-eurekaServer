948dfe213bddf985552bd87c2a65f53ec86c7567
Guilherme Vale <guilherme.vale98@gmail.com>
2025-03-01 15:12:52 -0300

    start eureka server

---
+++ src/main/java/com/gui/car_rental_eurekaServer/CarRentalEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalEurekaServerApplication.class, args);
+	}
+
+}
