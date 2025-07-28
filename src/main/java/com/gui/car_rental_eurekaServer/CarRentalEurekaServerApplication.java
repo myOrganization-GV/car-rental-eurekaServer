183eaf9a65ccb47cc25cb78d45091ddbc18f1edb
Guilherme Vale <guilherme.vale98@gmail.com>
2025-07-28 10:50:56 -0300

    add Jenkinsfile

--- src/main/java/com/gui/car_rental_eurekaServer/CarRentalEurekaServerApplication.java
+++ src/main/java/com/gui/car_rental_eurekaServer/CarRentalEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.gui.car_rental_eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CarRentalEurekaServerApplication {
 
 	public static void main(String[] args) {
